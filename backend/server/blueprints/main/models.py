37a31ff511d3ea9aa8e105829a636b2719500a2b
ethanbond64 <ethanbond39@gmail.com>
2022-05-13 20:57:04 -0600

    delete crap from old project

--- backend/server/blueprints/main/models.py
+++ backend/server/blueprints/main/models.py
@@ -1,49 +1,17 @@
 from datetime import datetime
-from server.utils.extensions import db
-from server.utils.BaseModel import BaseModel
-from sqlalchemy.dialects.postgresql import JSON
+from backend.server.utils.extensions import db
+from backend.server.utils.BaseModel import BaseModel
+# from sqlalchemy.dialects.postgresql import JSON
 
-class Event(BaseModel,db.Model):
 
-    __tablename__ = 'events'
+class Video(BaseModel, db.Model):
+
+    __tablename__ = 'videos'
     
     id = db.Column(db.Integer, primary_key=True)
     
-    name = db.Column(db.String(100))
-    about = db.Column(db.String(1000))
+    name = db.Column(db.String(64))
+    site = db.Column(db.String(64))
+    siteId = db.Column(db.String(64))
 
-    start_time = db.Column(db.DateTime(),default=datetime.now())
-    expiration = db.Column(db.DateTime())
-
-    # keys are names, values are types
-    # nested types nest
-    dataPolicy = db.Column(JSON)
-    # dataInPolicy = db.Column(JSON)
-    # dataOutPolicy = db.Column(JSON)
-
-    # Number of people that need to submit confirming data for it to be true
-    required_confirmations = db.Column(db.Integer)
-
-class Entry(BaseModel,db.Model):
-
-    __tablename__ = 'entries'
-
-    id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
-
-    data = db.Column(JSON)
-    timestamp = db.Column(db.DateTime(),index=True)
-    device_signature = db.Column(db.String(128))
-
-
-
-class Contract(BaseModel,db.Model):
-
-    __tablename__ = "contracts"
-
-    id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
-
-    env_json = db.Column(JSON)
-    filename = db.Column(db.String(100))
-    deployed = db.Column(db.Boolean,default=False)
+    # TODO add checkout related fields
