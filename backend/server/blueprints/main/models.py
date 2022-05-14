ac99b4c0318940d411130b563868deb6a443746d
ethanbond64 <ethanbond39@gmail.com>
2022-05-14 10:54:47 -0600

    add to data model

--- backend/server/blueprints/main/models.py
+++ backend/server/blueprints/main/models.py
@@ -12,4 +12,20 @@ class Video(BaseModel, db.Model):
     site = db.Column(db.String(64))
     siteId = db.Column(db.String(64))
 
+
+class Product(BaseModel, db.Model):
+
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    videoId = db.Column(db.Integer, db.ForeignKey("videos.id"), index=True, nullable=False)
+
+    name = db.Column(db.String(64))
+
+    # Time in seconds when the product first arrives on screen
+    timeEnter = db.Column(db.Integer)
+
+    # Time in seconds when the product first leaves the screen
+    timeExit = db.Column(db.Integer)
+
     # TODO add checkout related fields
--- backend/server/blueprints/main/views.py
+++ backend/server/blueprints/main/views.py
@@ -11,12 +11,18 @@ def get_test():
     return make_response(jsonify({"Test": True}), 200)
 
 
-@main.route('/fetch/video/<site>/<id>', methods=['GET'])
-def getVideoBySite(site, id):
-    if site.lower() == "youtube":
-        return make_response(jsonify({"Youtube ID": id}), 200)
+@main.route('/fetch/video/<site>/<siteId>', methods=['GET'])
+def getVideoBySite(site, siteId):
 
-    return make_response(jsonify({"Error": "Unknown site"}), 400)
+    try:
+        video = Video.query.filter(Video.site == site.lower(), Video.siteId == siteId).first()
+        if video is not None:
+            return make_response(jsonify({"Data": video.json()}), 200)
+
+    except:
+        return make_response(jsonify({"Error": "Error during fetch"}), 500)
+
+    return make_response(jsonify({"Error": "Unknown Video"}), 400)
 
 
 @main.route('/create/video', methods=['POST'])
@@ -25,9 +31,9 @@ def createVideo():
     try:
         data = request.get_json()
 
-        if  data.get('name') is not None and data.get('site') is not None and data.get('siteId') is not None:
+        if data.get('name') is not None and data.get('site') is not None and data.get('siteId') is not None:
             video = Video(**data).save()
-            return make_response(jsonify({"data": video.json()}), 200)
+            return make_response(jsonify({"Data": video.json()}), 200)
 
     except:
         return make_response(jsonify({"Error": "Error during save attempt"}), 500)
