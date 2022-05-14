1c25ab0affdb74c1122ee79beb16a0cef0fd7943
ethanbond64 <ethanbond39@gmail.com>
2022-05-14 11:22:36 -0600

    add index to db

--- backend/server/blueprints/main/models.py
+++ backend/server/blueprints/main/models.py
@@ -5,12 +5,20 @@ from backend.server.utils.BaseModel import BaseModel
 class Video(BaseModel, db.Model):
 
     __tablename__ = 'videos'
+    __table_args__ = (db.UniqueConstraint('site', 'siteId'), )
     
     id = db.Column(db.Integer, primary_key=True)
     
     name = db.Column(db.String(64))
     site = db.Column(db.String(64))
     siteId = db.Column(db.String(64))
+
+    # Override to include product list
+    def json(self):
+        products = [p.json() for p in Product.query.filter(Product.videoId == self.id).all()]
+        data = super().json()
+        data["products"] = products
+        return data
 
 
 class Product(BaseModel, db.Model):
