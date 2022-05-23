305812c1c888d73d0ad81793be2feb812ed93369
ethanbond64 <ethanbond39@gmail.com>
2022-05-23 11:54:23 -0600

    working

--- rapyd_auth.py
+++ rapyd_auth.py
@@ -14,11 +14,12 @@ import os
 load_dotenv()
 
 # TODO watch these commented out variables
-# http_method = 'post'                   # get|put|post|delete - must be lowercase
-http_method = 'get'                   # get|put|post|delete - must be lowercase
+http_method = 'post'                   # get|put|post|delete - must be lowercase
+# http_method = 'get'                   # get|put|post|delete - must be lowercase
 base_url = 'https://sandboxapi.rapyd.net'
-# path = '/v1/checkout'           # Portion after the base URL. Hardkeyed for this example.
-path = '/v1/data/countries'          
+path = '/v1/checkout'           # Portion after the base URL. Hardkeyed for this example.
+# path = '/v1/data/countries'   
+
 # salt: randomly generated for each request.
 min_char = 8
 max_char = 12
@@ -32,10 +33,16 @@ timestamp = calendar.timegm(d.utctimetuple())
 access_key = os.getenv('RAPYD_KEY')        # The access key received from Rapyd.
 secret_key = os.getenv('RAPYD_SECRET')        # Never transmit the secret key by itself.
 
-body = ''                             # JSON body goes here. Always empty string for GET; 
+body = {"amount":100,"country":"US","currency":"USD"}                             # JSON body goes here. Always empty string for GET; 
                                       # strip nonfunctional whitespace.
 
-to_sign = http_method + path + salt + str(timestamp) + access_key + secret_key + body
+if http_method == 'get':
+    body_to_sign = '' 
+else:
+    body_to_sign = json.dumps(body).replace(' ','')
+
+print(body_to_sign)
+to_sign = http_method + path + salt + str(timestamp) + access_key + secret_key + body_to_sign
 
 h = hmac.new(bytes(secret_key, 'utf-8'), bytes(to_sign, 'utf-8'), hashlib.sha256)
 
@@ -53,17 +60,10 @@ headers = {
 
 print(headers)
 
-url = "https://sandboxapi.rapyd.net/v1/checkout"
-
-body = {
-    "amount": 100,
-    "country": "US",
-    "currency": "USD"
-}
-
-# x = requests.post(url, data = body, headers = headers)
-
-x = requests.get("https://sandboxapi.rapyd.net/v1/data/countries", headers=headers)
+if http_method == 'post':
+    x = requests.post(url, data = body_to_sign, headers = headers)
+else:
+    x = requests.get(url, headers = headers)
 
 print(x)
 print(x.text)
