c0bca00b50cead721e1088286bb6c76f4f072052
ethanbond64 <ethanbond39@gmail.com>
2022-05-29 16:29:24 -0600

    delete from list

--- frontend/src/Video.js
+++ frontend/src/Video.js
@@ -1,14 +1,29 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Video(props) {
 
+    const [deleted, setDeleted] = useState(false);
+    let hider = deleted ? 'hidden' : '';
+
+    function deleteVideo() {
+        fetch(`http://localhost:8000/delete/video/${props.id}`, { method: 'GET', }).then(response => {
+            if (response.ok) {
+                response.json().then(json => {
+                    console.log(json);
+                    setDeleted(true);
+                });
+            }
+        });
+    };
+
     return (
-        <div className={`relative mb-6 p-2.5 rounded shadow-md h-250 bg-blue-100`}>
+        <div className={`relative mb-6 p-2.5 pb-16 rounded shadow-md h-fit bg-blue-100 ${hider}`}>
             <div className={`absolute rounded-t-lg inset-x-0 top-0 p-2.5 bg-indigo-900 text-white font-bold`}>
                 {props.name}
             </div>
             <br />
-            <img src={props.thumbnail} style={{ maxHeight: 500, maxWidth: 500 }} />
+            <img src={props.thumbnail} className={`max-h-54 mt-4 `} />
             <div class={`absolute rounded-b-lg inset-x-0 bottom-0 p-2.5 bg-white h-14`}>
                 <div className={`grid grid-cols-3 gap-4`}>
                     <a className={`inline-block border rounded py-1 px-3 bg-blue-400 text-white`} href={props.link}>
@@ -19,9 +34,9 @@ function Video(props) {
                             Register a product
                         </div>
                     </Link>
-                    <a className={`inline-block border rounded py-1 px-3 bg-blue-400 text-white`} href={`localhost:8000/V1/latest/${props.id}`}>
-                        TODO
-                    </a>
+                    <button className={`inline-block border rounded py-1 px-3 bg-red-400 hover:bg-red-700 text-white`} onClick={deleteVideo}>
+                        Delete
+                    </button>
                 </div>
             </div>
         </div>
